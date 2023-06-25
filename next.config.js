5f02321a0a0e94c0cc3eed46b4945a81de310127
Samarth Sheth <samarth.sheth.29@gmail.com>
2023-06-25 17:13:22 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["lh3.googleusercontent.com", "res.cloudinary.com", "task.com"],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["cloudinary", "graphql-request"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
+
+module.exports = nextConfig
